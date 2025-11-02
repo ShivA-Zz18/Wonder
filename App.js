f7fdaf6610b89bd3271106e83180deb874796a53
ShIVA <shiva959154@gmail.com>
2025-11-02 13:15:17 +0530

    new file app.js

---
+++ App.js
@@ -0,0 +1,101 @@
+// let a = 10;
+// let b = 30;
+
+// console.log(a+b);
+
+
+// let a= 10;
+// let b=20;
+ 
+
+// console.log("sun of 10 and 20 is :",(a+b));
+
+
+// let age = Number(prompt("Enter your age"));
+
+
+// console.log(age);
+
+
+
+// let a =10;
+// let b=20;
+
+// let c ;
+
+// c=a;
+// a=b;
+// b=c;
+
+
+// console.log(a);
+// console.log(b);
+
+
+// let a =10;
+// let b= 20;
+
+
+// a=a+b;//10+20=30
+
+// b=a-b;// 30-20=10
+
+// a=a-b; //30-10=20
+
+
+// console.log(a,b);
+
+
+
+// let a =10;
+// let b=20;
+
+// [a,b]=[b,a]
+
+
+// console.log(a,b);
+
+
+// let i = 11;
+
+// i=i++ ;
+
+// console.log(i);
+
+
+// let i =11;
+
+// i=++i;
+
+// console.log(i);
+
+
+// let a =11;
+// let b=22;
+// let c= a+b + a++ + b++ + ++a + ++b ;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// a=10;
+
+// // a=a+1;
+
+// a++;
+
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
